5a0609472fd1e4b9c729676695ca4668b337ffdd
marketingcd <marketing.cleverdetails@gmail.com>
2025-06-09 18:01:03 +0100

    Update appDataPT.ts

--- my-react-app/src/data/appDataPT.ts
+++ my-react-app/src/data/appDataPT.ts
@@ -680,7 +680,7 @@ export const appDataPT: AppData = {
             instagram: "https://www.instagram.com/aqualandalgarveoficial/",
             website: "https://www.aqualand.pt/"
           },
-          mapsUrl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d101808.49895831558!2d-8.355817944620606!3d37.116646827524605!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd1ad0e39d6bfd4b%3A0x81a56dd58df0e845!2sAqualand%20Algarve!5e0!3m2!1spt-PT!2spt!4v1749485410610!5m2!1spt-PT!2spt" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade",
+          mapsUrl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d101808.49895831558!2d-8.355817944620606!3d37.116646827524605!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd1ad0e39d6bfd4b%3A0x81a56dd58df0e845!2sAqualand%20Algarve!5e0!3m2!1spt-PT!2spt!4v1749485410610!5m2!1spt-PT!2spt",
           specialOffer: {
             type: "Discount",
             description: "28% de Desconto - Bilhete Adulto"
